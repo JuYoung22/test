89339c08b141f4952d71e824a9d7464eca73e6d3
JuYeung2 <bcd328@naver.com>
2023-09-10 13:06:30 +0900

    add: 로그인 구현 및 사용자 id 받아오기

--- logintest/server/server.js
+++ logintest/server/server.js
@@ -1,13 +1,20 @@
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 3001;
+const port = 3001;
 
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
-app.use('/api',(req,res) => res.json({username: 'bryan'}));
 
-app.listen(port,()=>{
-    console.log(`express is running on port ${port}`);
+app.post('/login', (req, res) => {
+  // 요청 처리 로직을 여기에 작성합니다.
+  // 클라이언트로 응답을 보내야 합니다.
+  // res.json(...) 또는 res.send(...) 등을 사용하세요.
+  const { inputId, inputPw } = req.body;
+  res.json({ inputId, inputPw });
+});
+
+app.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
 });
--- logintest/src/Login/modal.js
+++ logintest/src/Login/modal.js
@@ -1,14 +1,26 @@
 import React from "react";
 import './login.css';
 
-export default function Modal({setOpenModal}) {
-    const closeModal = () => {
-        setOpenModal(false);
-    };
+// export default function Modal({setOpenModal}) {
+//     const closeModal = () => {
+//         setOpenModal(false);
+//     };
 
-    return(
-        <div className="modalStyle">
-            <button onClick={closeModal}> 닫기 </button>
+//     return(
+//         <div className="modalStyle">
+//             <button onClick={closeModal}> 닫기 </button>
+//         </div>
+//     );
+// }
+function Modal({ setOpenModal, modalMessage }) {
+    return (
+      <div className="modalStyle">
+          {/* 응답데이터를 출력 */}
+          <p>{modalMessage}</p>
+          <button onClick={() => setOpenModal(false)}>닫기</button>
         </div>
     );
-}
+  }
+  
+
+  export default Modal;
